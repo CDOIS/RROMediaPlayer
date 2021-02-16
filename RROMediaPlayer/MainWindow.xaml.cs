6857c9db45085aea0cd791eb5de2c0a12a621f24
CDOIS <ricardocdois@gmail.com>
2021-02-15 22:20:04 -0400

    Add project files.

---
+++ RROMediaPlayer/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace RROMediaPlayer
+{
+    /// <summary>
+    /// Assignment1- Ricardo Oliveira - W048722 - 02/2021
+    /// Interaction logic for MainWindow.xaml
+    /// References:
+    /// https://wpf-tutorial.com/audio-video/how-to-creating-a-complete-audio-video-player/
+    /// https://youtu.be/k9Dm13xhrdY
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool mediaPlayerIsPlaying = false;
+        private bool userIsDraggingSlider = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            //UI
+            string ImgPath = $"{Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString()}\\Images\\MusicIcon.jpg";
+            Musicimg.Source = new BitmapImage(new Uri(ImgPath));
+
+            //Player
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Card_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+
+        }
+    }
+}
