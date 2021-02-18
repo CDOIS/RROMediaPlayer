2b57535eacc34d3f6a2a842990e5d55c2c44ba43
CDOIS <ricardocdois@gmail.com>
2021-02-17 23:28:35 -0400

    Update -> Almost finished - Only left - Custom Control

--- RROMediaPlayer/MainWindow.xaml.cs
+++ RROMediaPlayer/MainWindow.xaml.cs
@@ -26,13 +26,16 @@ namespace RROMediaPlayer
     /// https://youtu.be/k9Dm13xhrdY
     /// MusicIcon = https://images.app.goo.gl/J3uFhzXuFmDxdJ3r5 (Free sourcing)
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window    
     {
         private bool mediaPlayerIsPlaying = false;
         private bool userIsDraggingSlider = false;
+        TagLib.File file;
+
         public MainWindow()
         {
             InitializeComponent();
+
             ////UI
             //string ImgPath = $"{Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.ToString()}\\Images\\MusicIcon.png";
             //Musicimg.Source = new BitmapImage(new Uri(ImgPath));
@@ -64,10 +67,7 @@ namespace RROMediaPlayer
             Application.Current.Shutdown();
         }
 
-        private void CommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
-        {
 
-        }
 
         private void Open_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
@@ -77,12 +77,35 @@ namespace RROMediaPlayer
         private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
+            
             openFileDialog.Filter = "Media Files (*.mp3;*.mpg;*.mpeg)|*.mp3;*.mpg;*.mpeg|All files (*.*)|*.*";
-            if(openFileDialog.ShowDialog() == true)
+
+            try
             {
-                mePlayer.Source = new Uri(openFileDialog.FileName);
+                if (openFileDialog.ShowDialog() == true)
+                {
+                    mePlayer.Source = new Uri(openFileDialog.FileName);
+
+                    file = TagLib.File.Create(openFileDialog.FileName);
+
+                    if (file.Tag.Title is null)
+                    {
+                        labelSongName.Text = openFileDialog.FileName;
+                    } else
+                    {
+                        labelSongName.Text = (file.Tag.Title + "; " + file.Tag.FirstPerformer);
+                        labelSongAtt.Text = (file.Tag.Album + "; " + file.Tag.Year);
+                    }
+
+                    
+                }
             }
+            catch (Exception ex) {
+                MessageBox.Show("A handled exception just occurred: " + ex.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+            };
+            
         }
+
         private void Play_CanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
             e.CanExecute = (mePlayer != null) && (mePlayer.Source != null);
@@ -133,6 +156,41 @@ namespace RROMediaPlayer
         private void Grid_MouseWheel(object sender, MouseWheelEventArgs e)
         {
             mePlayer.Volume += (e.Delta > 0) ? 0.1 : -0.1;
+        }
+
+        private void btnTagEdit_Click(object sender, RoutedEventArgs e)
+        {
+            editorPanel.Visibility = Visibility.Visible;
+            if (file.Tag.Title == "")
+            {
+                eTitleTag.Text = labelSongName.Text;
+                eArtistTag.Text = "Please update Tag";
+                eAlbumTag.Text = "Please update Tag";
+                eYearTag.Text = "Please update Tag";
+            }
+            eTitleTag.Text = (file.Tag.Title);
+            eArtistTag.Text = (file.Tag.FirstPerformer);
+            eAlbumTag.Text = (file.Tag.Album);
+            eYearTag.Text = (file.Tag.Year).ToString();
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                file.Tag.Title = eTitleTag.Text;
+                file.Tag.Performers = new String[1] { eArtistTag.Text };
+                file.Tag.Album = eAlbumTag.Text;
+                file.Tag.Year = Convert.ToUInt32(eYearTag.Text);
+                file.Save();
+                labelSongName.Text = (file.Tag.Title + "; " + file.Tag.FirstPerformer);
+                labelSongAtt.Text = (file.Tag.Album + "; " + file.Tag.Year);
+                editorPanel.Visibility = Visibility.Hidden;
+            } catch ( Exception ex )
+            {
+                MessageBox.Show("A handled exception just occurred: " + ex.Message, "Exception Sample", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
         }
     }
 }
